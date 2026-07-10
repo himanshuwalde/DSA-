5285de8c20bb1df4f4c5e667847a1808d052ec0e
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-10 20:16:37 +0530

    Update E-14. Longest Common Prefix.java

--- Strings/E-14. Longest Common Prefix.java
+++ Strings/E-14. Longest Common Prefix.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder ans = new StringBuilder();
+        Arrays.sort(strs);
+        int n = strs.length;
+        String s1 = strs[0];
+        String s2 = strs[n-1];
+        for(int i=0; i<Math.min(s1.length(), s2.length()); i++){
+            if(s1.charAt(i) != s2.charAt(i)){
+                return ans.toString();
+            }
+            ans.append(s1.charAt(i));
+        }
+        return ans.toString();
+    }
+}
