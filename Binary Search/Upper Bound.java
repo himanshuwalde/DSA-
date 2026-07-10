6d62fb764b1bccb94750fb5b6fb8298367703e7f
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-10 21:51:45 +0530

    Create Upper Bound.java

---
+++ Binary Search/Upper Bound.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int upperBound(int[] nums, int x) {
+        int n = nums.length;
+       int i=0, j=n-1;
+       while(i<=j){
+        int mid = (i+j)/2;
+        if(nums[mid] <= x){
+            i = mid+1;
+        }
+        else if(nums[mid] > x){
+            j = mid-1;
+        }
+       }
+       return i;
+    }
+}
