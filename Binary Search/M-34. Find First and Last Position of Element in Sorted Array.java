4ee1f7d366f94b6d7ce80526fae867f889741f4e
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-10 20:29:17 +0530

    Update M-34. Find First and Last Position of Element in Sorted Array.java

--- Binary Search/M-34. Find First and Last Position of Element in Sorted Array.java
+++ Binary Search/M-34. Find First and Last Position of Element in Sorted Array.java
@@ -1,1 +1,30 @@
-
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+        int[] arr = new int[2];
+        int i = 0, j = n-1;
+        while(i<=j){
+            int mid = (i+j)/2;
+            if(nums[mid] >= target){
+                j = mid-1;
+            }
+            else if(nums[mid] < target){
+                i = mid+1;
+            }
+        }
+        arr[0] = (i<n && nums[i] == target)? i : -1;
+        i = 0;
+        j = n-1;
+        while(i<=j){
+            int mid = (i+j)/2;
+            if(nums[mid] > target){
+                j = mid-1;
+            }
+            else if(nums[mid] <= target){
+                i = mid+1;
+            }
+        }
+        arr[1] = (j>=0 && nums[j] == target)? j : -1;
+        return arr;
+    }
+}
