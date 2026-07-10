e0d797129b30cccdf01d1b15e12a4964c2f08fcf
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-11 00:44:07 +0530

    Create E-83. Remove Duplicates from Sorted List.java

---
+++ Linked List/1-D Linked List/E-83. Remove Duplicates from Sorted List.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode ptr1 = head;
+        ListNode ptr2 = head;
+        while(ptr2 != null){
+           if(ptr1.val == ptr2.val){
+            ptr2 = ptr2.next;
+           }
+           else{
+            ptr1.next = ptr2;
+            ptr1 = ptr2;
+           }
+        }
+        ptr1.next = ptr2;
+        return head;
+    }
+}
