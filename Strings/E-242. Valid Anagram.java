c8042aa1be0946abd2acdedc4d36280cd6a1a041
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-10 20:59:14 +0530

    Create E-242. Valid Anagram.java

---
+++ Strings/E-242. Valid Anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        if(arr1.length != arr2.length){
+            return false;
+        }
+        int i = 0;
+        while(i < arr1.length){
+            if(arr1[i] != arr2[i]){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+}
