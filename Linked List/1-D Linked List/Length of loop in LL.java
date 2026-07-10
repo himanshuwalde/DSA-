d6942a08c0b5e17a0b9e22838457e448314b7428
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-10 21:22:30 +0530

    Create Length of loop in LL.java

---
+++ Linked List/1-D Linked List/Length of loop in LL.java
@@ -0,0 +1,46 @@
+/*Definition of singly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+ */
+
+ class Solution {
+     public int findLengthOfLoop(ListNode head) {
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                ListNode a = head;
+                while(a != slow){
+                    a = a.next;
+                    slow = slow.next;
+                }
+                int count = 1;
+                ListNode temp = slow;
+                while(slow.next != temp){
+                    count++;
+                    slow = slow.next;
+                }
+                return count;
+            }
+        }
+        return 0;
+     }
+ }
