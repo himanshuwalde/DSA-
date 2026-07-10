67ffefb3e1ca1c50ae9487bd2fa0834fbbccf724
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-10 23:53:31 +0530

    Update E-704. Binary Search.java

--- Binary Search/E-704. Binary Search.java
+++ Binary Search/E-704. Binary Search.java
@@ -1,21 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length;
-        return binarySearch(nums, target, 0, n-1); 
-    }
-    public int binarySearch(int[] nums, int target, int low, int high) {
-        int mid = low + (high - low)/2;
-        if(low>high){
-            return -1;
+        int i = 0;
+        int j = nums.length-1;
+        while(i<=j) {
+            int mid = (i+j)/2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            else if(nums[mid]>target) {
+                j = mid - 1;
+            }
+            else {
+                i = mid + 1;
+            }
         }
-        if(nums[mid] == target){
-            return mid;
-        }
-        else if(nums[mid] > target){
-            return binarySearch(nums, target, low, mid-1);
-        }
-        else{
-            return binarySearch(nums, target, mid+1, high);
-        }
+        return -1;
     }
 }
