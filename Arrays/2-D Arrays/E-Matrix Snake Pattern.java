2e4133bcd5682c5e8f9585b51b4f8d2a808fe549
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-11 00:24:28 +0530

    Create E-Matrix Snake Pattern.java

---
+++ Arrays/2-D Arrays/E-Matrix Snake Pattern.java
@@ -0,0 +1,19 @@
+class Solution {
+    static ArrayList<Integer> snakePattern(int matrix[][]) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int n = matrix.length;
+        for(int i=0; i<n; i++){
+            if(i%2 == 0){
+                for(int j=0; j<n; j++){
+                    ans.add(matrix[i][j]);
+                }
+            }
+            else{
+                for(int j=n-1; j>=0; j--){
+                    ans.add(matrix[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
+}
