c770ed127566fb452b2eea1c3578e36ae748c98c
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-12 00:08:12 +0530

    Update E-144. Binary Tree Preorder Traversal.java

--- Binary Tree/E-144. Binary Tree Preorder Traversal.java
+++ Binary Tree/E-144. Binary Tree Preorder Traversal.java
@@ -28,3 +28,26 @@ class Solution {
         helper(root.right, ans);
     }
 }
+
+// ITERATIVE APPROACH:
+// class Solution {
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         if(root == null){
+//             return ans;
+//         }
+//         Stack<TreeNode> st = new Stack<>();
+//         st.push(root);
+//         while(st.size() > 0){
+//             TreeNode top = st.pop();
+//             ans.add(top.val);
+//             if(top.right != null){
+//                 st.push(top.right);
+//             }
+//             if(top.left != null){
+//                 st.push(top.left);
+//             }   
+//         }
+//         return ans;
+//     }
+// }
