db259c4770001e34c4d146fcb95f90a9ec534ac6
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-11 11:05:43 +0530

    Update E-1470. Shuffle the Array.java

--- Arrays/1-D Arrays/E-1470. Shuffle the Array.java
+++ Arrays/1-D Arrays/E-1470. Shuffle the Array.java
@@ -2,10 +2,10 @@ class Solution {
     public int[] shuffle(int[] nums, int n) {
         int i=0, j=n, k=0;
         int[] arr = new int[2*n];
-       while(j < 2*n){
-        arr[k++] = nums[i++];
-        arr[k++] = nums[j++];
-       }
-       return arr;     
+        while(j < 2*n){
+            arr[k++] = nums[i++];
+            arr[k++] = nums[j++];
+        }
+        return arr;     
     }
 }
