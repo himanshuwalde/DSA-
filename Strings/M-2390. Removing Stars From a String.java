f8bf0979337681c4f547a1dac5dc549842513178
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-11 21:05:33 +0530

    Update M-2390. Removing Stars From a String.java

--- Strings/M-2390. Removing Stars From a String.java
+++ Strings/M-2390. Removing Stars From a String.java
@@ -1,23 +1,39 @@
 class Solution {
     public String removeStars(String s) {
-        Stack<Character> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
         int n = s.length();
         for(int i=0; i<n; i++){
             if(s.charAt(i) != '*'){
-                st.push(s.charAt(i));
+                sb.append(s.charAt(i));
             }
             else{
-                st.pop();
+                sb.deleteCharAt(sb.length()-1);
             }
         }
-        Stack<Character> st2 = new Stack<>();
-        while(st.size() > 0){
-            st2.push(st.pop());
-        }
-        String ans = "";
-        while(st2.size() > 0){
-            ans += st2.pop();
-        }
-        return ans;
+        return sb.toString();
     }
 }
+
+// class Solution {
+//     public String removeStars(String s) {
+//         Stack<Character> st = new Stack<>();
+//         int n = s.length();
+//         for(int i=0; i<n; i++){
+//             if(s.charAt(i) != '*'){
+//                 st.push(s.charAt(i));
+//             }
+//             else{
+//                 st.pop();
+//             }
+//         }
+//         Stack<Character> st2 = new Stack<>();
+//         while(st.size() > 0){
+//             st2.push(st.pop());
+//         }
+//         String ans = "";
+//         while(st2.size() > 0){
+//             ans += st2.pop();
+//         }
+//         return ans;
+//     }
+// }
