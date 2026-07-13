431a282411764bc257846f11d6c34a613deb3c83
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-13 19:34:35 +0530

    Update M-98. Validate Binary Search Tree.java

--- Binary Search Tree/M-98. Validate Binary Search Tree.java
+++ Binary Search Tree/M-98. Validate Binary Search Tree.java
@@ -13,23 +13,52 @@
  *     }
  * }
  */
-class Solution {
-    public boolean isValidBST(TreeNode root) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        inorder(root, ans);
-        for(int i=0; i<ans.size()-1; i++){
-            if(ans.get(i) >= ans.get(i+1)){
-                return false;
-            }
-        }
-        return true;
-    }
-    public void inorder(TreeNode root,  ArrayList<Integer> ans) {
-        if(root == null){
-            return;
-        }
-        inorder(root.left, ans);
-        ans.add(root.val);
-        inorder(root.right, ans);
+
+class Triplet {
+    long max;
+    long min;
+    boolean isBST;
+    Triplet (long max, long min, boolean isBST){
+        this.max = max;
+        this.min = min;
+        this.isBST = isBST;
     }
 }
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root).isBST;
+    }
+    public Triplet helper(TreeNode root) {
+       if(root == null){ 
+        return new Triplet(Long.MIN_VALUE, Long.MAX_VALUE, true);
+       }
+       Triplet lst = helper(root.left);
+       Triplet rst = helper(root.right);
+       long rootVal = root.val;
+       long max = Math.max(rootVal, Math.max(lst.max, rst.max));
+       long min = Math.min(rootVal, Math.min(lst.min, rst.min));
+       boolean isBST = (lst.isBST) && (rst.isBST) && (lst.max < root.val) && (rst.min > root.val);
+       return new Triplet(max, min, isBST);
+    }
+}
+
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         ArrayList<Integer> ans = new ArrayList<>();
+//         inorder(root, ans);
+//         for(int i=0; i<ans.size()-1; i++){
+//             if(ans.get(i) >= ans.get(i+1)){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     public void inorder(TreeNode root,  ArrayList<Integer> ans) {
+//         if(root == null){
+//             return;
+//         }
+//         inorder(root.left, ans);
+//         ans.add(root.val);
+//         inorder(root.right, ans);
+//     }
+// }
