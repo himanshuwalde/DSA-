7afb4448664580cc4671fa7bd42066ec21a6f16b
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-13 18:41:14 +0530

    Update M-75. Sort Colors.java

--- Arrays/1-D Arrays/M-75. Sort Colors.java
+++ Arrays/1-D Arrays/M-75. Sort Colors.java
@@ -1,18 +1,44 @@
+// DUTCH NATIONAL FLAG ALGORITHM
 class Solution {
     public void sortColors(int[] nums) {
         int n = nums.length;
-        for(int i = 0; i<n; i++) {
-            int min = i;
-            for(int j = i+1; j<=n-1; j++) {
-                if(nums[min] > nums[j]) {
-                    min = j;
-                }
-            }
-            if(min!=i) {
+        int i = 0, mid = 0, j = n-1;
+        while(mid <= j){
+            if(nums[mid] == 0){
                 int temp = nums[i];
-                nums[i] = nums[min];
-                nums[min] = temp;
+                nums[i] = nums[mid];
+                nums[mid] = temp;
+                i++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                int temp = nums[mid];
+                nums[mid] = nums[j];
+                nums[j] = temp;
+                j--;
             }
         }
     }
 }
+
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         int n = nums.length;
+//         for(int i = 0; i<n; i++) {
+//             int min = i;
+//             for(int j = i+1; j<=n-1; j++) {
+//                 if(nums[min] > nums[j]) {
+//                     min = j;
+//                 }
+//             }
+//             if(min!=i) {
+//                 int temp = nums[i];
+//                 nums[i] = nums[min];
+//                 nums[min] = temp;
+//             }
+//         }
+//     }
+// }
