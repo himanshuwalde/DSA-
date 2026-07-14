a688fa28610d9cb3ddab443ac2d3758679560b26
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-14 20:20:13 +0530

    Update E-94. Binary Tree Inorder Traversal.java

--- Binary Tree/E-94. Binary Tree Inorder Traversal.java
+++ Binary Tree/E-94. Binary Tree Inorder Traversal.java
@@ -13,21 +13,52 @@
  *     }
  * }
  */
+
+// MORRIS TRAVERSAL:
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        helper(root, ans); 
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.left != null){
+                TreeNode pred = curr.left;
+                while(pred.right != null && pred.right != curr){
+                    pred = pred.right;
+                }
+                if(pred.right == null){
+                    pred.right = curr;
+                    curr = curr.left;
+                }
+                else{
+                    pred.right = null;
+                    ans.add(curr.val);
+                    curr = curr.right;
+                }
+            }else{
+                ans.add(curr.val);
+                curr = curr.right;
+            }
+        }
         return ans;
     }
-    public void helper(TreeNode root, List<Integer> ans) {
-        if(root == null){
-            return;
-        }
-        helper(root.left, ans);
-        ans.add(root.val);
-        helper(root.right, ans);
-    }
 }
+
+// RECURSIVE APPROACH:
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         helper(root, ans); 
+//         return ans;
+//     }
+//     public void helper(TreeNode root, List<Integer> ans) {
+//         if(root == null){
+//             return;
+//         }
+//         helper(root.left, ans);
+//         ans.add(root.val);
+//         helper(root.right, ans);
+//     }
+// }
 
 // ITERATIVE APPROACH:
 // class Solution {
