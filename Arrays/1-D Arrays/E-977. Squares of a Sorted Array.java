0d4dbec3b34bde783d227fcb35d1218383092130
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-14 19:36:29 +0530

    Update E-977. Squares of a Sorted Array.java

--- Arrays/1-D Arrays/E-977. Squares of a Sorted Array.java
+++ Arrays/1-D Arrays/E-977. Squares of a Sorted Array.java
@@ -1,10 +1,30 @@
-class Solution {
+class Solution {    // TC: O(n)
     public int[] sortedSquares(int[] nums) {
         int n = nums.length;
-        for(int i=0; i<n; i++){
-            nums[i] = nums[i] * nums[i];
+        int[] arr = new int[n];
+        int i = 0, j = n-1, pos = n-1;
+        while(i <= j){
+            if(Math.abs(nums[i]) > Math.abs(nums[j])){
+                arr[pos] = nums[i] * nums[i];
+                i++;
+            }
+            else{
+                arr[pos] = nums[j] * nums[j];
+                j--;
+            }
+            pos--;
         }
-        Arrays.sort(nums);
-        return nums;
+        return arr;
     }
 }
+
+// class Solution {    // TC: O(n*logn)
+//     public int[] sortedSquares(int[] nums) {
+//         int n = nums.length;
+//         for(int i=0; i<n; i++){
+//             nums[i] = nums[i] * nums[i];
+//         }
+//         Arrays.sort(nums);
+//         return nums;
+//     }
+// }
