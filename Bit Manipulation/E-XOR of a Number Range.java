db289674b678bc470bb242d96c36cd8083e6fd00
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-20 00:12:26 +0530

    Create E-XOR of a Number Range.java

---
+++ Bit Manipulation/E-XOR of a Number Range.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static int findXOR(int l, int r) {
+        return XOR(l-1) ^ XOR(r);
+    }
+    public static int XOR(int n) {
+        if(n % 4 == 1){
+            return 1;
+        }
+        else if(n % 4 == 2){
+            return n+1;
+        }
+        else if(n % 4 == 3){
+            return 0;
+        }
+        else{
+            return n;
+        }
+    }
+}
