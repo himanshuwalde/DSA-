cec3d49c7d5fcf4031767df29779caade0225c1f
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-19 23:32:26 +0530

    Update E-Power of 2.java

--- Bit Manipulation/E-Power of 2.java
+++ Bit Manipulation/E-Power of 2.java
@@ -1,10 +1,25 @@
 class Solution {
     public static boolean isPowerofTwo(int n) {
-        for(int i=0; i<=n/2; i++){
-            if(1 << i == n){
-                return true;
-            }
+        if(n == 0){
+            return false;
         }
-        return false;
+        while(n > 1){
+            if(n%2 == 1){
+                return false;
+            }
+            n = n/2;
+        }
+        return true;
     }
 }
+
+// class Solution {
+//     public static boolean isPowerofTwo(int n) {
+//         for(int i=0; i<=n/2; i++){
+//             if(1 << i == n){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
