4f4ed9de2d6408f0dd77ea12e57cae5db4f19458
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-19 23:25:30 +0530

    Update E-Set the rightmost unset bit.java

--- Bit Manipulation/E-Set the rightmost unset bit.java
+++ Bit Manipulation/E-Set the rightmost unset bit.java
@@ -3,3 +3,14 @@ class Solution {
         return n | (n+1);
     }
 }
+
+// class Solution {
+//     public int setBit(int n) {
+//         for(int i=0; i<31; i++){
+//             if((n >> i)%2 == 0){
+//                 return n | (1 << i);
+//             }
+//         }
+//         return n;
+//     }
+// }
