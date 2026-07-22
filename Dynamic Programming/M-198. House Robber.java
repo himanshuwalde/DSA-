c3f92b0a338ca6158060df0f307a9fd4e4b9bae4
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-22 16:49:58 +0530

    Update M-198. House Robber.java

--- Dynamic Programming/M-198. House Robber.java
+++ Dynamic Programming/M-198. House Robber.java
@@ -1,15 +1,36 @@
-class Solution {    // Tabulation
+class Solution {    // Space Optimised Tabulation
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        if(n >1) dp[1] = Math.max(nums[0], nums[1]);
-        for(int i=2; i<n; i++){
-            dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
+        if(n == 1){
+            return nums[0];
         }
-        return dp[n-1];
+        if(n == 2){
+            return Math.max(nums[0], nums[1]);
+        }
+        int[] dp = new int[3];
+        dp[0] = nums[0];
+        if(n > 1) dp[1] = Math.max(nums[0], nums[1]);
+        for(int i=2; i<n; i++){
+            dp[2] = Math.max(nums[i] + dp[0], dp[1]);
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+        }
+        return dp[2];
     }
 }
+
+// class Solution {    // Tabulation
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         int[] dp = new int[n];
+//         dp[0] = nums[0];
+//         if(n > 1) dp[1] = Math.max(nums[0], nums[1]);
+//         for(int i=2; i<n; i++){
+//             dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
+//         }
+//         return dp[n-1];
+//     }
+// }
 
 // class Solution {    // Memoization
 //     static int[] dp;
