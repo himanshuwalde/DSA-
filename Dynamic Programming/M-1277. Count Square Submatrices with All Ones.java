0b502b8e6d5b98fc9ce5a9f04d6fc27c1f451a6e
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-07-23 09:46:53 +0530

    Create M-1277. Count Square Submatrices with All Ones.java

---
+++ Dynamic Programming/M-1277. Count Square Submatrices with All Ones.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int count = 0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i != 0 && j != 0){
+                    if(matrix[i][j] == 1){
+                        matrix[i][j] = matrix[i][j] + Math.min(matrix[i][j-1], Math.min(matrix[i-1][j], matrix[i-1][j-1]));
+                    }
+                }
+                count += matrix[i][j];
+            }
+        }
+        return count;
+    }
+}
