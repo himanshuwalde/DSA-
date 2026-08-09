37faeccadb4923f03939f501af75e90466dd2639
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-08-09 23:12:18 +0530

    Create E-125. Valid Palindrome.java

---
+++ Strings/E-125. Valid Palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if(Character.isLetterOrDigit(ch)){
+                sb.append(Character.toLowerCase(ch));
+            }
+        }
+        String s1 = sb.toString();
+        String s2 = sb.reverse().toString();
+        return s1.equals(s2);
+    }
+}
