70f46a8c65e87f3273daccc062afc7ecebafba45
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-08-15 12:51:21 +0530

    Update H-188. Best Time to Buy and Sell Stock IV.java

--- Dynamic Programming/H-188. Best Time to Buy and Sell Stock IV.java
+++ Dynamic Programming/H-188. Best Time to Buy and Sell Stock IV.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution { // MEMOIZATION
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
         k = k * 2;  // k transactions means 'k' times buy & sell, hence k *= 2
@@ -17,14 +17,38 @@ class Solution {
         }
         if(k%2 == 0){  // will buy when k value is even
             int buy = helper(prices, n, i+1, k-1, dp) - prices[i];
-            int pass = helper(prices, n, i+1, k, dp);
-            dp[i][k] = Math.max(buy, pass);
+            int skip = helper(prices, n, i+1, k, dp);
+            dp[i][k] = Math.max(buy, skip);
         }
         else{  // will sell when k value is odd
             int sell = helper(prices, n, i+1, k-1, dp) + prices[i];
-            int pass = helper(prices, n, i+1, k, dp);
-            dp[i][k] = Math.max(sell, pass);
+            int skip = helper(prices, n, i+1, k, dp);
+            dp[i][k] = Math.max(sell, skip);
         }
         return dp[i][k];
     }
 }
+
+// class Solution {    // TABULATION
+//     public int maxProfit(int k, int[] prices) {
+//         int n = prices.length;
+//         int[][] dp = new int[n + 1][2*k + 1];
+//         for (int i = n - 1; i >= 0; i--) {
+//             for (int j = 1; j <= k*2; j++) {
+//                 if (j%2 == 0) {
+//                     // BUY
+//                     int buy = dp[i + 1][j - 1] - prices[i];
+//                     int skip = dp[i + 1][j];
+//                     dp[i][j] = Math.max(buy, skip);
+//                 } 
+//                 else {
+//                     // SELL
+//                     int sell = dp[i + 1][j - 1] + prices[i];
+//                     int skip = dp[i + 1][j];
+//                     dp[i][j] = Math.max(sell, skip);
+//                 }
+//             }
+//         }
+//         return dp[0][2*k];
+//     }
+// }
