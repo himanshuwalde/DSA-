990149ea1c800d4b07d7f9f2e9716d535aca544e
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-08-18 19:09:54 +0530

    Update E-860. Lemonade Change.java

--- Greedy/E-860. Lemonade Change.java
+++ Greedy/E-860. Lemonade Change.java
@@ -1,1 +1,30 @@
-1
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int n = bills.length;
+        int fives = 0;
+        int tens = 0;
+        //int twenties = 0;
+        for(int i=0; i<n; i++){
+            if(bills[i] == 5) fives++;
+            else if(bills[i] == 10){
+                tens++;
+                if(fives >= 1){
+                    fives--;
+                }
+                else return false;
+            }
+            else{
+                //twenties++;
+                if(tens >= 1 && fives >= 1){
+                    tens--;
+                    fives--;
+                }
+                else if(fives >= 3){
+                    fives -= 3;
+                }
+                else return false;
+            }   
+        }
+        return true;
+    }
+}
