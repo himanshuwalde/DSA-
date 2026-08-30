0dd61b35a4b326df0f78f6f541e537a915592930
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-08-30 21:31:57 +0530

    Create E-2379. Minimum Recolors to Get K Consecutive Black Blocks.java

---
+++ Sliding Window/E-2379. Minimum Recolors to Get K Consecutive Black Blocks.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumRecolors(String blocks, int k) {
+        int n = blocks.length();
+        int minOperations = Integer.MAX_VALUE;
+        for(int i=0; i<=n-k; i++){
+            int whiteCount = 0;
+
+            // Count the number of 'W's in the current window
+            for(int j=i; j<i+k; j++){
+                if(blocks.charAt(j) == 'W'){
+                    whiteCount++;
+                }
+            }
+            minOperations = Math.min(minOperations, whiteCount);
+        }
+        return minOperations;
+    }
+}
