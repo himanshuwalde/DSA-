e6e5aa6a071a1b7a70000bd6591cd275a57fa98f
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-09-05 18:18:54 +0530

    Create M-3904. Smallest Stable Index II.java

---
+++ Prefix Sum/M-3904. Smallest Stable Index II.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+        int[] prefMax = new int[n];
+        prefMax[0] = nums[0];
+        for(int i=1; i<n; i++){
+            prefMax[i] = Math.max(prefMax[i-1], nums[i]);
+        }
+        int[] suffMin = new int[n];
+        suffMin[n-1] = nums[n-1];
+        for(int i=n-2; i>=0; i--){
+            suffMin[i] = Math.min(suffMin[i+1], nums[i]);
+        }
+        int[] stability = new int[n];
+        for(int i=0; i<n; i++){
+            stability[i] = prefMax[i] - suffMin[i];
+            if(stability[i] <= k){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
