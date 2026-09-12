11761eb7541aff8fe1bb9159bd87abf255fd51ce
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-09-12 13:44:29 +0530

    Create E-2180. Count Integers With Even Digit Sum.java

---
+++ Math/E-2180. Count Integers With Even Digit Sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countEven(int num) {
+        int count = 0;
+        for(int i=2; i<=num; i++){
+            int sum = 0;
+            int temp = i;
+            while(temp != 0){
+                sum += temp%10;
+                temp = temp/10;
+            }
+            if(sum % 2 == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
