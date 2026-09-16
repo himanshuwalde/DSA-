3ac8d3459b77b0f9620b7249058a2d4405027018
himanshuwalde <148128440+himanshuwalde@users.noreply.github.com>
2026-09-17 00:10:25 +0530

    Create E-3300. Minimum Element After Replacement With Digit Sum.java

---
+++ Math/E-3300. Minimum Element After Replacement With Digit Sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minElement(int[] nums) {
+        for(int i=0; i<nums.length; i++){
+            int sum = 0;
+            while(nums[i] != 0){
+                sum += nums[i] % 10;
+                nums[i] /= 10;
+            }
+            nums[i] = sum;
+        }
+        Arrays.sort(nums);
+        return nums[0];
+    }
+}
